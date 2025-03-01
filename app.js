4bda3896cdd26e6694c8b8a8b8d612489f845659
Klaus-DBgit config --global user.email klausnt@gmail.com <klausnt@gmail.com>
2025-03-01 16:00:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo"); 
+    let nombreAmigo = inputAmigo.value;
+    if(!nombreAmigo){
+        alert("Debes escribir un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus;
+    
+}
